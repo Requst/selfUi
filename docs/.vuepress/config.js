570303ae9ee531a026ed87c726c8cb7e9238212b
陈桂文 <937494210@qq.com>
2022-01-26 11:42:53 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,12 @@
 module.exports = {
+    title: "self-Ui4",
     base: '/selfUi/',
     themeConfig: {
+      nav: [
+        {text:"Home", link: "/",},
+        {text:"Github", link: "https://github.com/Requst/selfUi"},
+        {text:"Npm", link: "https://www.npmjs.com/package/self-ui4"},
+      ],
       sidebar: [
         '/',
         '/componentDocs/card'
